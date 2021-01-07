009c29987a2d7817bd27f3c7def4858aa42bdc1e
Paul Chaignon <paul@cilium.io>
2021-01-07 22:19:02 +0100

    test: Enable additional tests to run with kube-proxy on newer kernels
    Now that we have a CI pipeline able to run both with kube-proxy and our
    kube-proxy replacement, we can enable additional tests (mostly
    IPSec-related) on newer kernels by explicitely disabling our kube-proxy
    replacement.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -1921,27 +1921,24 @@ Secondary Interface %s :: IPv4: (%s, %s), IPv6: (%s, %s)`, helpers.DualStackSupp
 			})
 		})
 
-		SkipContextIf(helpers.RunsWithKubeProxyReplacement, "", func() {
+		SkipContextIf(helpers.RunsWithoutKubeProxy, "With kube-proxy", func() {
 			BeforeAll(func() {
-				if helpers.RunsWithHostFirewall() {
-					DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
-						// When kube-proxy is enabled, the host firewall is not
-						// compatible with externalTrafficPolicy=Local because traffic
-						// from pods to remote nodes goes through the tunnel.
-						// This issue is tracked at #12542.
-						"hostFirewall": "false",
-					})
-				}
+				DeployCiliumOptionsAndDNS(kubectl, ciliumFilename, map[string]string{
+					"kubeProxyReplacement": "disabled",
+					// When kube-proxy is enabled, the host firewall is not
+					// compatible with externalTrafficPolicy=Local because traffic
+					// from pods to remote nodes goes through the tunnel.
+					// This issue is tracked at #12542.
+					"hostFirewall": "false",
+				})
 			})
 
-			It("Tests NodePort (kube-proxy) with externalTrafficPolicy=Local", func() {
+			It("Tests NodePort with externalTrafficPolicy=Local", func() {
 				testExternalTrafficPolicyLocal()
 			})
 
 			AfterAll(func() {
-				if helpers.RunsWithHostFirewall() {
-					DeployCiliumAndDNS(kubectl, ciliumFilename)
-				}
+				DeployCiliumAndDNS(kubectl, ciliumFilename)
 			})
 		})
 
